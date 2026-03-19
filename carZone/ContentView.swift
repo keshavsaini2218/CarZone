5ccafc331be47b7c733567786369a122afbd4543
keshavsaini2218 <keshavsaini2218@gmail.com>
2026-03-20 00:32:58 +0530

    Initial Commit

---
+++ carZone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  carZone
+//
+//  Created by KESHAV  on 20/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ carZone/carZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  carZoneApp.swift
+//  carZone
+//
+//  Created by KESHAV  on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct carZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
